db80bb27bd90396c7c179432e772a22d8c18e24e
mushfiqurahman-git <mymensingh456@gmail.com>
2024-09-25 09:37:46 -0400

    first commit

--- app/src/main/java/com/example/todowearapp/model/Task.java
+++ app/src/main/java/com/example/todowearapp/model/Task.java
@@ -1,4 +1,28 @@
 package com.example.todowearapp.model;
 
 public class Task {
+    private String id = "";
+
+    private String taskDetails ="";
+
+    public Task(String id, String taskDetails) {
+        this.id = id;
+        this.taskDetails = taskDetails;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTaskDetails() {
+        return taskDetails;
+    }
+
+    public void setTaskDetails(String taskDetails) {
+        this.taskDetails = taskDetails;
+    }
 }
