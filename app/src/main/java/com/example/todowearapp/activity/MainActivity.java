a6b01b44a6ad93e77f00b008d9dabbcc657342ac
mushfiqurahman-git <mymensingh456@gmail.com>
2024-09-25 10:45:31 -0400

    utils /Constants.java

--- app/src/main/java/com/example/todowearapp/activity/MainActivity.java
+++ app/src/main/java/com/example/todowearapp/activity/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.todowearapp.activity;
 
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +15,64 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.todowearapp.R;
+import com.example.todowearapp.databinding.ActivityMainBinding;
+import com.example.todowearapp.model.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+        init();
     }
+    private void init() {
+        //setLayout();
+        initTask();
+    }
+    private void initTask(){
+        mainBinding.edtTask.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId== EditorInfo.IME_ACTION_SEND)
+                {
+                    String text = v.getText().toString();
+                    if(!TextUtils.isEmpty(text))
+                    {
+                        Task task = createTask(null,text);
+                        updateTask(task,Constants.ACTION_ADD);
+                        v.setText("");
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+    }
+    private Task createTask(String id,String task){
+        if(id == null){
+            id = String.valueOf(System.currentTimeMillis());
+        }
+        return new Task(id,task);
+    }
+    private void updateTask(Task task,int action){
+        if(action == Constants.ACTION_ADD){
+            TaskUtils.savgeTask(task,this);
+            ConfirmUtils.showSaveMessage(getString(R.string.task_saved),this);
+        }
+    }
+
+
 }
+
---
+++ app/src/main/java/com/example/todowearapp/utils/Constants.java
@@ -0,0 +1,4 @@
+package com.example.todowearapp.utils;
+
+public class Constants {
+}
